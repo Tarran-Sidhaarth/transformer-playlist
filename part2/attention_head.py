3e9dd2b7496a5afcea42a60143bfc7d3e4659ca6
Tarran-Sidhaarth <tarransidhaarth@gmail.com>
2026-06-05 22:18:41 +0000

    implemented attention head

--- part2/attention_head.py
+++ part2/attention_head.py
@@ -9,3 +9,25 @@ generator.manual_seed(42)
 CONTEXT_LENGTH = 4
 EMBEDDING_DIMENSION = 6
 
+X = torch.randn(CONTEXT_LENGTH,EMBEDDING_DIMENSION, generator=generator)
+
+WQ = nn.Linear(EMBEDDING_DIMENSION,EMBEDDING_DIMENSION, bias=False)
+WK = nn.Linear(EMBEDDING_DIMENSION,EMBEDDING_DIMENSION, bias=False)
+WV = nn.Linear(EMBEDDING_DIMENSION,EMBEDDING_DIMENSION, bias=False)
+
+Q = WQ(X) # X @ WQ
+K = WK(X)
+V = WV(X)
+
+print(f"Q,K,V shapes: {Q.shape}, {K.shape}, {V.shape}")
+
+# K(context_length, embedding_dimensions) -> K(embedding_dimension, context_length)
+
+similarity = (Q @ K.transpose(0,1))/math.sqrt(EMBEDDING_DIMENSION)
+normalized_score = similarity.softmax(-1)
+
+output = normalized_score @ V
+
+print(normalized_score)
+print(output.shape)
+
